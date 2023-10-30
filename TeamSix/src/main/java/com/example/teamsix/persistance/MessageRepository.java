a51292609be6c938bd376452b4e41e8fec54de37
Burak <brk_seyrek@outlook.com>
2023-10-30 14:05:31 +0100

    Sprint Aufgabe 1 - v0.1

--- TeamSix/src/main/java/com/example/teamsix/persistance/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.teamsix.persistance;
-
-import com.example.teamsix.domain.Message;
-import org.springframework.data.repository.ListCrudRepository;
-
-
-public interface MessageRepository extends ListCrudRepository<Message,Long> {
-}
-
--- TeamSix/src/main/java/com/example/teamsix/persistance/PortfolioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.teamsix.persistance;
-
-import com.example.teamsix.domain.Portfolio;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface PortfolioRepository extends ListCrudRepository<Portfolio,Long> {
-}
