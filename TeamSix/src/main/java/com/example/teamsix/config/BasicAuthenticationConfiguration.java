e7ef72edf529e3d48c25a5e632f1249b67098bb6
Ufuk Altin <ufuk.altin@kiongroup.com>
2023-12-08 15:49:05 +0100

    New config for cors

--- TeamSix/src/main/java/com/example/teamsix/config/BasicAuthenticationConfiguration.java
+++ TeamSix/src/main/java/com/example/teamsix/config/BasicAuthenticationConfiguration.java
@@ -48,8 +48,9 @@ public class BasicAuthenticationConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
